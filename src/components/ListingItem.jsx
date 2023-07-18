b4bebe2a47f7b95a5fd2fc5cbcdf9d0371adbed6
Phan Anh <phananh12042003@gmail.com>
2023-07-18 05:19:58 -0400

    add list in profile page 🚀

---
+++ src/components/ListingItem.jsx
@@ -0,0 +1,5 @@
+function ListingItem({ listing, id }) {
+  return <div>{listing.name}</div>;
+}
+
+export default ListingItem;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,26 @@
 import { getAuth, updateProfile } from "firebase/auth";
-import { doc, updateDoc } from "firebase/firestore";
-import { useState } from "react";
+import {
+  collection,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
 import { FcHome } from "react-icons/fc";
 import { Link } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -51,6 +62,28 @@ function Profile() {
       toast.error("Could not upload profile details ❌");
     }
   };
+
+  useEffect(() => {
+    async function fetchUserListings() {
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
 
   return (
     <>
@@ -115,6 +148,22 @@ function Profile() {
           </button>
         </div>
       </seciton>
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
+            <ul>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                ></ListingItem>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </>
   );
 }
