ac736c822ac3007e57684eac23bccfdb8db5019c
Phan Anh <phananh12042003@gmail.com>
2023-07-18 05:41:00 -0400

    styling listing item 🚀

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -64,6 +64,7 @@ function Profile() {
   };
 
   useEffect(() => {
+    //get listing from database
     async function fetchUserListings() {
       const listingRef = collection(db, "listings");
       const q = query(
@@ -152,7 +153,7 @@ function Profile() {
         {!loading && listings.length > 0 && (
           <>
             <h2 className="text-2xl text-center font-semibold">My Listings</h2>
-            <ul>
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
