86aea8929cd9194734d3cf20c3109714d0b91e73
Phan Anh <phananh12042003@gmail.com>
2023-07-18 16:57:11 -0400

    share copy functionality 🚀

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -26,6 +26,7 @@ function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
   SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -64,6 +65,24 @@ function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+
+      <div
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+      >
+        <FaShare className="text-lg font-light text-slate-500"></FaShare>
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2 text-green-600">
+          Link copied ✅
+        </p>
+      )}
     </main>
   );
 }
