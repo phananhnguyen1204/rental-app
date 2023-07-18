ba540251ce36357309bc7d01935e7808938401f8
Phan Anh <phananh12042003@gmail.com>
2023-07-17 22:25:20 -0400

    styling profile 🚀

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,51 @@
+import { useState } from "react";
+
 function Profile() {
-  return <div>Profile</div>;
+  const [formData, setFormData] = useState({
+    name: "Phan Anh",
+    email: "phananh12042003@gmail.com",
+  });
+  const { name, email } = formData;
+  return (
+    <>
+      <seciton className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            {/* name input */}
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            ></input>
+
+            {/* email input */}
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            ></input>
+
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p className="flex items-center">
+                Do you want to change your name?{" "}
+                <span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                  Edit
+                </span>
+              </p>
+              <p className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer">
+                Sign out
+              </p>
+            </div>
+          </form>
+        </div>
+      </seciton>
+    </>
+  );
 }
 
 export default Profile;
