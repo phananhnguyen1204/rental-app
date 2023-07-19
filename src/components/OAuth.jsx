190ba2e072e93ec29343b2a036e5ff1ce3d59b15
Phan Anh <phananh12042003@gmail.com>
2023-07-19 02:09:23 -0400

    fix toast🚀

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -29,7 +29,7 @@ function OAuth() {
           timestamp: serverTimestamp(),
         });
       }
-      toast.error("Sign up with Google successfully ✅");
+      toast.success("Sign up with Google successfully ✅");
       navigate("/");
     } catch (error) {
       toast.error("Could not sign up with Google");
