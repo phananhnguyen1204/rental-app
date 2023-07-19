fdaf41373afe6c7873fdf4773847354534d0b803
Phan Anh <phananh12042003@gmail.com>
2023-07-18 21:30:01 -0400

    plaec for sale🚀

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -79,6 +79,39 @@ function Home() {
     fetchListings();
   }, []);
 
+  //Sale
+  const [saleListings, setSaleListings] = useState(null);
+  useEffect(() => {
+    async function fetchListings() {
+      try {
+        //get reference
+        const listingsRef = collection(db, "listings");
+        //create a query (limit, or condition of request)
+        //only request listings if the offer is true
+        const q = query(
+          listingsRef,
+          where("type", "==", "sale"),
+          orderBy("timestamp", "desc"),
+          limit(4)
+        );
+        //execute query
+        const querySnap = await getDocs(q);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setSaleListings(listings);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+
+    fetchListings();
+  }, []);
+
   return (
     <div>
       <Slider></Slider>
@@ -103,7 +136,7 @@ function Home() {
             </ul>
           </div>
         )}
-        {/* Show Offers */}
+        {/* Places for rent */}
         {rentListings && rentListings.length > 0 && (
           <div className="m-2 mb-6">
             <h2 className="px-3 text-2xl mt-6 font-semibold">
@@ -116,6 +149,27 @@ function Home() {
             </Link>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               {rentListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                ></ListingItem>
+              ))}
+            </ul>
+          </div>
+        )}
+        {/* Places for sale */}
+        {saleListings && saleListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">
+              Places for sale
+            </h2>
+            <Link to="/category/sale">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-700 duration-150 ease-in-out">
+                Show more places for sale
+              </p>
+            </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {saleListings.map((listing) => (
                 <ListingItem
                   key={listing.id}
                   listing={listing.data}
