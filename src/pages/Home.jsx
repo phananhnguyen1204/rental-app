bee1b641515c4b46ce41d8122dfd67366647853a
Phan Anh <phananh12042003@gmail.com>
2023-07-18 20:49:15 -0400

    styling swiper🚀

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import Slider from "../components/Slider";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Slider></Slider>
+    </div>
+  );
 }
 
 export default Home;
