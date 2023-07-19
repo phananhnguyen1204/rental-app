295bc6f47674833d8a695873f9a2357f53d9549c
Phan Anh <phananh12042003@gmail.com>
2023-07-18 22:02:45 -0400

    offers page 🚀

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,5 +1,119 @@
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  where,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import ListingItem from "../components/ListingItem";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase";
+
 function Offers() {
-  return <div></div>;
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null);
+
+  useEffect(() => {
+    async function fetchListings() {
+      try {
+        const listingRef = collection(db, "listings");
+
+        const q = query(
+          listingRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(8)
+        );
+
+        const querySnap = await getDocs(q);
+        //get the last one
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchedListing(lastVisible);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fetch listing offer");
+      }
+    }
+    fetchListings();
+  }, []);
+
+  const fetchMoreListings = async () => {
+    try {
+      const listingRef = collection(db, "listings");
+
+      const q = query(
+        listingRef,
+        where("offer", "==", true),
+        orderBy("timestamp", "desc"),
+        startAfter(lastFetchedListing),
+        limit(4)
+      );
+
+      const querySnap = await getDocs(q);
+      //get the last one
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+      const listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error("Could not fetch listing offer");
+    }
+  };
+
+  return (
+    <div className="max-w-6xl mx-auto px-3">
+      <h1 className="text-3xl text-center mt-6 font-bold mb-6">Offers</h1>
+      {loading ? (
+        <Spinner></Spinner>
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                ></ListingItem>
+              ))}
+            </ul>
+          </main>
+          {lastFetchedListing && (
+            <div className="flex justify-center items-center">
+              <button
+                onClick={fetchMoreListings}
+                className="bg-blue-400 hover:bg-blue-600 px-3 py-1.2 text-black hover:text-white border border-gray-300 mb-6 mt-6 hover:border-slate-400 rounded transition duration-150 ease-in-out"
+              >
+                Load more
+              </button>
+            </div>
+          )}
+        </>
+      ) : (
+        <p>There are no current offers</p>
+      )}
+    </div>
+  );
 }
 
 export default Offers;
